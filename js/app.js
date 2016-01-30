5f694fc3122915066ba2ee8814a5fed9ce358c42
River <prateergamer@gmail.com>
2016-01-30 08:32:50 +0530

    Display the cat's name,image and label.

---
+++ js/app.js
@@ -0,0 +1,22 @@
+var ViewModel = function(){
+	this.clickCount = ko.observable(0);
+	this.name = ko.observable("Snow White");
+	this.imgSrc= ko.observable("images/cat.jpg");
+	this.imgAttribution = ko.observable("https://flickr.com/user43");
+	this.label = ko.computed(function(){
+		var label;
+		if(this.clickCount() < 10){
+			label = "Newborn";
+		}else if(this.clickCount() < 30){
+			label = "Infant";
+		}else{
+			label = "Teen";
+		};
+		return label;
+	},this);
+	this.incrementCounter = function(){
+		this.clickCount(this.clickCount() + 1);
+	}
+}
+
+ko.applyBindings(new ViewModel());
